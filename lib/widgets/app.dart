bc2d7a6c269772942a55947a68b3357f4cc0b1b4
Pdom <ahb0327@naver.com>
2021-09-01 09:34:36 +0900

    Update app

--- lib/widgets/app.dart
+++ lib/widgets/app.dart
@@ -1,34 +1,42 @@
-import 'package:flutter/widgets.dart';
+import 'package:doter/controllers/core_controller.dart';
+import 'package:doter/controllers/document_controller.dart';
+import 'package:doter/widgets/color_picker.dart';
+import 'package:doter/widgets/menu_bar.dart';
+import 'package:doter/widgets/status_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-import 'a_panel.dart';
-import 'b_panel.dart';
-import 'layout.dart';
+import 'workspace.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return WidgetsApp(
-      title: 'doter',
-      color: const Color(0xFFFFFFFF),
-      debugShowCheckedModeBanner: false,
-      builder: (context, int) {
-        return Layout(
-          registedWidgets: {
-            'a': Builder(builder: (context) => APanel()),
-            'b': Builder(builder: (context) => BPanel())
-          },
-          config: LayoutConfig(
-              child: LayoutItemConfig(type: LayoutItemType.row, children: [
-            LayoutItemConfig(type: LayoutItemType.widget, widgetName: 'a'),
-            LayoutItemConfig(type: LayoutItemType.widget, widgetName: 'b'),
-            LayoutItemConfig(type: LayoutItemType.column, children: [
-              LayoutItemConfig(type: LayoutItemType.widget, widgetName: 'a'),
-              LayoutItemConfig(type: LayoutItemType.widget, widgetName: 'b'),
-              LayoutItemConfig(type: LayoutItemType.widget, widgetName: 'a'),
-            ]),
-          ])),
-        );
-      },
-    );
+    Get.put(CoreController());
+    Get.put(DocumentController());
+
+    return MaterialApp(
+        title: 'doter',
+        color: const Color(0xFFFFFFFF),
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            body: MenuBar(
+          child: Column(
+            children: [
+              Expanded(
+                child: Row(
+                  children: [
+                    Flexible(
+                        flex: 4,
+                        child: ConstrainedBox(
+                            constraints: BoxConstraints.expand(),
+                            child: Workspace())),
+                    Flexible(flex: 1, child: ColorPicker())
+                  ],
+                ),
+              ),
+              StatusBar()
+            ],
+          ),
+        )));
   }
 }
---
+++ lib/widgets/color_picker.dart
@@ -0,0 +1,66 @@
+import 'package:doter/controllers/core_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class ColorPicker extends StatefulWidget {
+  const ColorPicker({Key? key}) : super(key: key);
+
+  @override
+  _ColorPickerState createState() => _ColorPickerState();
+}
+
+class _ColorPickerState extends State<ColorPicker> {
+  var r = 255.0;
+
+  final CoreController coreController = Get.find();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Obx(() {
+        var primaryColor = coreController.primaryColor.value;
+
+        return Column(
+          children: [
+            Container(
+              height: 100,
+              width: 100,
+              color: primaryColor,
+            ),
+            Text(primaryColor.toString()),
+            Slider(
+                min: 0,
+                max: 255,
+                value: primaryColor.red.toDouble(),
+                onChanged: _onChangedRedSlider),
+            Slider(
+                min: 0,
+                max: 255,
+                value: primaryColor.green.toDouble(),
+                onChanged: _onChangedGreenSlider),
+            Slider(
+                min: 0,
+                max: 255,
+                value: primaryColor.blue.toDouble(),
+                onChanged: _onChangedBlueSlider)
+          ],
+        );
+      }),
+    );
+  }
+
+  void _onChangedRedSlider(double value) {
+    coreController.primaryColor.value =
+        coreController.primaryColor.value.withRed(value.toInt());
+  }
+
+  void _onChangedGreenSlider(double value) {
+    coreController.primaryColor.value =
+        coreController.primaryColor.value.withGreen(value.toInt());
+  }
+
+  void _onChangedBlueSlider(double value) {
+    coreController.primaryColor.value =
+        coreController.primaryColor.value.withBlue(value.toInt());
+  }
+}
