9def3cfed919b80daafc3ec841d384fd5c97a318
Pdom <ahb0327@naver.com>
2021-10-02 19:47:19 +0900

    Update menu_bar.dart

--- lib/widgets/menu_bar.dart
+++ lib/widgets/menu_bar.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:menubar/menubar.dart';
@@ -9,40 +11,42 @@ class MenuBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    setApplicationMenu([
-      Submenu(label: 'File', children: [
-        MenuItem(
-            label: 'New File',
-            enabled: true,
-            shortcut:
-                LogicalKeySet(LogicalKeyboardKey.meta, LogicalKeyboardKey.keyN),
-            onClicked: () async {
-              await showDialog(
-                  context: context,
-                  builder: (context) => SimpleDialog(
-                        title: const Text('New File'),
-                        children: [
-                          TextField(),
-                          SimpleDialogOption(
-                            onPressed: () {
-                              Navigator.pop(context, '');
-                            },
-                            child: const Text('확인'),
-                          )
-                        ],
-                      ));
-            }),
-        MenuDivider(),
-        Submenu(label: 'Presets', children: [
+    if (Platform.isWindows || Platform.isMacOS || Platform.isLinux) {
+      setApplicationMenu([
+        Submenu(label: 'File', children: [
           MenuItem(
-              label: 'Red',
+              label: 'New File',
               enabled: true,
-              shortcut: LogicalKeySet(LogicalKeyboardKey.meta,
-                  LogicalKeyboardKey.shift, LogicalKeyboardKey.keyR),
-              onClicked: () {}),
-        ])
-      ]),
-    ]);
+              shortcut: LogicalKeySet(
+                  LogicalKeyboardKey.meta, LogicalKeyboardKey.keyN),
+              onClicked: () async {
+                await showDialog(
+                    context: context,
+                    builder: (context) => SimpleDialog(
+                          title: const Text('New File'),
+                          children: [
+                            TextField(),
+                            SimpleDialogOption(
+                              onPressed: () {
+                                Navigator.pop(context, '');
+                              },
+                              child: const Text('확인'),
+                            )
+                          ],
+                        ));
+              }),
+          MenuDivider(),
+          Submenu(label: 'Presets', children: [
+            MenuItem(
+                label: 'Red',
+                enabled: true,
+                shortcut: LogicalKeySet(LogicalKeyboardKey.meta,
+                    LogicalKeyboardKey.shift, LogicalKeyboardKey.keyR),
+                onClicked: () {}),
+          ])
+        ]),
+      ]);
+    }
 
     return Container(
       child: child,
